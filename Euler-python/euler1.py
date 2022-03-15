83fc320545d12dfe508bacd68e62f7a5466ea4bf
Ali Sharify <87280202+alisharifyy@users.noreply.github.com>
2022-03-15 13:41:53 +0330

    Update euler1.py

--- Euler-python/euler1.py
+++ Euler-python/euler1.py
@@ -1,8 +1,8 @@
+#the resualt variable
 counter = 0
 
-
 for i in range(1000):
-    #check for number with % oprator
+    #check for number multiples to 3 or 5 whit % oprator
     if (i % 3 == 0 or i % 5 == 0):
         #if we found that number so we added to counter variable
         counter += i
