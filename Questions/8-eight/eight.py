a880e10e92326f50ff4914601a3ed4e33b55ad5a
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-13 10:34:36 -0600

    added file

---
+++ Questions/8-eight/eight.py
@@ -0,0 +1,19 @@
+import sys
+
+# get input from user
+tmp = input("Enter character: ")
+
+# get ascii value of character
+tmp = ord(tmp)
+
+# if tmp between 48 to 57 so user input is number
+if (tmp >= 48 and tmp <= 57):
+    print("number")
+
+# if tmp between 65 to 90 so user input is number
+elif(tmp >= 65 and tmp <= 90):
+    print("upper case alphbet")
+
+# if tmp between 97 to 122 so user input is number
+elif(tmp >= 97 and tmp <= 122):
+    print("Lower case alphabet")        
