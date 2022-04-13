90e0d6f02888a01fb0380d3082b156934f81bb91
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-13 09:03:10 -0600

    Add files via upload

--- Questions/5-Five/Five.py
+++ Questions/5-Five/Five.py
@@ -1,1 +1,29 @@
-""" --  """
+import sys
+
+def is_even(number):
+    if (number % 2 == 0):
+        return True
+    return False    
+
+
+
+n = int(input("Enter numbr: "))
+if (n < 0):
+    print("Number must be positive :(")
+    sys.exit(1)
+
+while (True):
+    print(f"Number is {n}")
+    if (n == 1):
+        print(f"End number is {n}")
+        sys.exit(0)
+
+    if (is_even(n)):
+        n //= 2
+        continue
+
+    n *= 3
+    n += 1     
+
+
+
