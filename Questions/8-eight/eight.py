11512bcec3dcdcf817c55a74f69bb4c2d69f5593
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-13 21:19:57 +0430

    Update eight.py

--- Questions/8-eight/eight.py
+++ Questions/8-eight/eight.py
@@ -9,15 +9,21 @@ tmp = ord(tmp)
 # if tmp between 48 to 57 so user input is number
 if (tmp >= 48 and tmp <= 57):
     print("number")
+    sys.exit(0)
 
 # if tmp between 65 to 90 so user input is number
 elif(tmp >= 65 and tmp <= 90):
     print("upper case alphbet")
-
+    sys.exit(0)
+    
 # if tmp between 97 to 122 so user input is number
 elif(tmp >= 97 and tmp <= 122):
     print("Lower case alphabet")        
-
+    sys.exit(0)
+    
+else:
+    print("its a symbol")
+    sys.exit(0)
 
     # ascii table 
 # https://www.asciitable.com/asciifull.gif 
